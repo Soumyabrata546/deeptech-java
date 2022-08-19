d96260d29ba1067d2ba1fdb8a88270091b4bfcfb
Soumyabrata Chatterjee <61630257+Soumyabrata546@users.noreply.github.com>
2022-08-19 12:40:35 +0530

    Create 2.java

---
+++ 2.java
@@ -0,0 +1,15 @@
+public class dptech3 {
+    public static void main(String args[]){
+        int n1=37;
+        if(n1%2==0){
+        if(n1<0)
+        System.out.println(n1+"is even and negative");
+        else
+        System.out.println(n1+"is even but not negative");
+        
+    }else
+    System.out.println(n1+"is neither even nor negative");
+    
+        
+}
+}
