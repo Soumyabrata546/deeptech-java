4c4881bb7b47cdea38960c02972b2bb5ff821880
Soumyabrata Chatterjee <61630257+Soumyabrata546@users.noreply.github.com>
2022-08-19 12:43:31 +0530

    Add files via upload

---
+++ deeptech2.java
@@ -0,0 +1,13 @@
+public class deeptech2 {
+    public static void main(String args[]){
+        int n1=30,n2=50;
+        if(n1>n2){
+            System.out.println(n1+"is larger");
+        }
+        else{
+            System.out.println(n2+"is larger");
+
+        }
+        
+}
+}
