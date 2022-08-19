6c6736236a671689d8a3e7719cc5a6381939991a
Soumyabrata Chatterjee <61630257+Soumyabrata546@users.noreply.github.com>
2022-08-19 13:12:08 +0530

    Add files via upload

---
+++ thr.java
@@ -0,0 +1,10 @@
+public class thr{
+    public static void main(String args[]){
+        int a=90;
+        if(a%5==0&&a%3==0){
+            System.out.println("It is divisable by 3 and 5");
+        }else
+        System.out.println("It is not divisable by 3 and 5");
+
+        }
+}
