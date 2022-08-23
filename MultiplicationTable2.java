fe34d43677168bbb1b677198943bb886951585ea
Soumyabrata Chatterjee <61630257+Soumyabrata546@users.noreply.github.com>
2022-08-23 12:50:54 +0530

    Write a java program to print the table of all the numbers from 1 to 10

---
+++ MultiplicationTable2.java
@@ -0,0 +1,19 @@
+package firstPackage;
+
+import java.util.Scanner;
+
+public class MultiplicationTable2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		for(int j=1;j<=10;j++) {
+			System.out.println("\nTable of "+j+"\n");
+		for(int i=1;i<=10;i++) {
+			System.out.println(j+" * "+i+" = "+j*i);
+		}
+		
+		}
+	}
+
+}
