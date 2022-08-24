17f241b91dff5082641f6d1629d15bf38730cc85
Soumyabrata Chatterjee <61630257+Soumyabrata546@users.noreply.github.com>
2022-08-24 13:52:27 +0530

    Add files via upload

---
+++ Sumstring.java
@@ -0,0 +1,13 @@
+package firstPackage;
+
+public class Sumstring {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String n1="25",n2="32";
+		int a1,a2;
+		System.out.print(n1+n2);
+		
+	}
+
+}
