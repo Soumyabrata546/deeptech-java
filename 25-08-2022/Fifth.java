f4fffe5b8d40c8e1a182d321388154ae293b3ef2
Soumyabrata Chatterjee <61630257+Soumyabrata546@users.noreply.github.com>
2022-08-26 00:19:58 +0530

    Update Fifth.java

--- 25-08-2022/Fifth.java
+++ 25-08-2022/Fifth.java
@@ -1,5 +1,11 @@
 package Second;
-
+// Create a class called Employee that includes three pieces of information as instance variables—a first name
+// (typeString), a last name (typeString) and a monthly salary (double). Your class should have a constructor that
+// initializes the three instance variables. If the monthly
+// salary is not positive, set it to 0.0. Write a test application named EmployeeTest that demonstrates class
+// Employee’s capabilities. Create two Employee objects and display each object’s yearly salary. Then give each
+// Employe
+// Employee a 10% raise and display each Employee’s yearly salary again.
 public class Fifth {
 
 	public static void main(String[] args) {
