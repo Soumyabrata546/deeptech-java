62fa957da34e63e4d1a7e76d2fbfdc92e49a6110
Soumyabrata Chatterjee <61630257+Soumyabrata546@users.noreply.github.com>
2022-08-26 12:07:22 +0530

    Create Fourt.java

---
+++ 25-08-2022/invoice/Fourt.java
@@ -0,0 +1,20 @@
+package Second;
+
+import java.util.Scanner;
+public class Fourth {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		String pnumber,pdesc;
+		int quantity;
+		double price;
+		System.out.println("Enter part no , part description, quantity and price per item=");
+		pnumber=sc.next();
+		pdesc=sc.next();
+		quantity=sc.nextInt();
+		price=sc.nextDouble();
+		Invoice ob1=new Invoice(pnumber,pdesc,quantity,price);
+		System.out.println("Amount="+ob1.getInvoice());
+	}
+}
