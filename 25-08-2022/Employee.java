9f910731b62250241cb10a789b0f5c11692b5ba9
Soumyabrata Chatterjee <61630257+Soumyabrata546@users.noreply.github.com>
2022-08-26 11:20:55 +0530

    Update Employee.java

--- 25-08-2022/Employee.java
+++ 25-08-2022/Employee.java
@@ -4,11 +4,12 @@ public class Employee {
 		String firstname,lastname;
 		double salary;
 		Employee(String fname,String lname,double salary){
-			if(salary<0) {
-				this.salary=0.0;
-			}
+			
 			firstname=fname;
 			lastname=lname;
 			this.salary=salary;
+			if(salary<0) {
+				this.salary=0.0;
+			}
 		}
 	}
