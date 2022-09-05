c0b050eb64647dda1d647b49e7b2cf672f823f06
Soumyabrata Chatterjee <61630257+Soumyabrata546@users.noreply.github.com>
2022-09-05 12:41:14 +0530

    Update second.java

--- 05-09-22/Ques-1/second.java
+++ 05-09-22/Ques-1/second.java
@@ -5,15 +5,21 @@ package Fresh;
 // If temperature <5, throw exception “TooCold”.
 //Otherwise, print “Normal” and convert it to Farenheit.
 public class second {
+	
 	static void checktemp(double temp)throws TooHot,TooCold {
 		if(temp>35)
 			throw new TooHot("To hot temperataure");
-		else
+		else if(temp<5)
 			throw new TooCold("To Cold");
+		else {
+			System.out.print("Normal");
+			double farenhite=(temp*9/5)+32;
+			System.out.println("\nTemperature in Frenhite="+farenhite+"F");
+		}
 	}
 public static void main(String args[]) {
 	try {
-		second.checktemp(45);
+		second.checktemp(10);
 	} catch (TooHot e) {
 		System.out.print(e.getMessage());
 	} catch (TooCold e) {
