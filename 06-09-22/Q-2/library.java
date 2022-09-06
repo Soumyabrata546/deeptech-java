738fa9c3b9ae6465d63a5ee4033909fcf83f9f8e
Soumyabrata Chatterjee <61630257+Soumyabrata546@users.noreply.github.com>
2022-09-06 14:20:02 +0530

    Update library.java

--- 06-09-22/Q-2/library.java
+++ 06-09-22/Q-2/library.java
@@ -1,14 +1,16 @@
 package Fresh;
 
-class library{
+class library {
 	String books[]= {"Ansi C","Overview of java","C++ Programming"};
 	String auth_names[]= {"E.balaguruswami","E.balaguruswami","yashwant kanetkar"};
 	void check(String b_name) throws BookNotFoundException {
 		for(int i=0;i<books.length;i++) {
-			if(b_name.equals(books[i]))
+			if(!(b_name.equals(books[i]))) {
+				throw new BookNotFoundException("book not found");
+			}
+			else {
 				System.out.print(books[i]+" - "+auth_names[i]);
-			else
-				throw new BookNotFoundException("not found");
+			}
 		}
 	}
 }
