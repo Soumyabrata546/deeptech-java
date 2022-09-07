90f53506eb15b7f7e8073ee8e757ce21c8bfe6cc
Soumyabrata Chatterjee <61630257+Soumyabrata546@users.noreply.github.com>
2022-09-07 13:29:28 +0530

    Update StudentBusinessLogic.java

--- 07-09-22/Unit Testing - 2/StudentBusinessLogic.java
+++ 07-09-22/Unit Testing - 2/StudentBusinessLogic.java
@@ -3,8 +3,8 @@ package com.java.unit.unit1;
 public class StudentBusinessLogic {
 double calpercentage(StudentDetails s1) {
 	double percen;
-	percen=(s1.getMarks1()+s1.getMarks2()+s1.getMarks3())/3;
-	s1.setParcentage(percen);
+	int total_marks=300;
+	percen=(s1.getMarks1()+s1.getMarks2()+s1.getMarks3())/total_marks*100;
 	return percen;
 	
 	}
