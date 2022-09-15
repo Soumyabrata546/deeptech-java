9f57cb75c85323820aad34856e2b3fac9c60f9fc
Soumyabrata Chatterjee <61630257+Soumyabrata546@users.noreply.github.com>
2022-09-15 13:06:45 +0530

    Update firstandlastOccur.java

--- 15-09-22/Ques-1/firstandlastOccur.java
+++ 15-09-22/Ques-1/firstandlastOccur.java
@@ -6,9 +6,18 @@ public class firstandlastOccur {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String s1="Hello world";
-		int findex=s1.indexOf('o');
-		int lindex=s1.lastIndexOf('o');
-		System.out.print(findex+" "+lindex);
+		for(int i=0;i<s1.length();i++) {
+			if(s1.charAt(i)=='o') {
+				System.out.print("First occurance="+i);
+				break;
+			}
+		}
+		for(int i=s1.length()-1;i>=0;i--) {
+			if(s1.charAt(i)=='o') {
+				System.out.print("First occurance="+i);
+				break;	
+			}
+		}
 	}
 
 }
