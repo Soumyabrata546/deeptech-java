2b53aae2c5f49136a542d3b00245efd6c6c414a7
Soumyabrata Chatterjee <61630257+Soumyabrata546@users.noreply.github.com>
2022-09-17 13:14:48 +0530

    Add files via upload

---
+++ 17-09-22/Quest-3/File7.java
@@ -0,0 +1,24 @@
+package com.java.unit.unit1;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class File7 {
+	
+//WAP to count the number of letters in a file
+	
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		FileInputStream fin=new FileInputStream("test.txt");
+		int i,count=0;
+		
+		while((i=fin.read())!=-1) {
+		if(Character.isLetter(i)) {
+			count++;
+		}		
+		}
+		System.out.print(count);
+	}
+
+}
